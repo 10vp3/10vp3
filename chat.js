ab48a9c59906ec97a29666bef7bce75f8189e8b7
10vp3 <cyprieng920@gmail.com>
2024-08-11 20:12:05 +0200

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,16 @@
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 // Fonction pour récupérer le nom d'utilisateur depuis localStorage
 function getUsername() {
-    const username = localStorage.getItem('username');
+    const username = getCookie("username");
     if (username) {
         return username;
     } else {
